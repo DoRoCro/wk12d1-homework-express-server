a3f2f3062374d8943b0f409d6bc04afe83062095
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-17 22:52:16 +0100

    un-broke server

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get('/', function (req, res) {
 })
 
 app.use(express.static('client/build'))
-app.use(require('./client/src/controllers'))                // says look for index.js in controllers directory.
-app.use(require('./client/src/models'))                // says look for index.js in controllers directory.
+// app.use(require('./client/src/controllers'))                // says look for index.js in controllers directory.
+// app.use(require('./client/src/models'))                // says look for index.js in controllers directory.
 
 var server = app.listen(3000, function () {
   var host = server.address().address
